6e253b520a9d47ee8741080fa2e5b593192f5378
Malcolm <hmrasch@misena.edu.co>
2019-12-11 11:30:23 -0500

    cambio

--- src/ciclos/hacer/mientras/Masa_de_aire.java
+++ src/ciclos/hacer/mientras/Masa_de_aire.java
@@ -1,9 +1,14 @@
 
 package ciclos.hacer.mientras;
 
+import java.util.Scanner;
+
 public class Masa_de_aire {
 
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int vehiculos=0;
+        
         
     }
     
